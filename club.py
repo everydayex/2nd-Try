d60277344a68779955db88650ce8f8b8d4dc521b
Haji Dukureh <hdukereh6@gmail.com>
2019-09-14 11:39:42 +0000

    Added a club entrance

---
+++ club.py
@@ -0,0 +1,12 @@
+age = int(input("Enter your age : "))
+
+if age >= 21:
+    print("over the age limit, you can enter")
+elif age == 21:
+    print("At legal age")
+elif age <= 21:
+    print("Under age, you cannot enter")
+else:
+    print("you cannot enter")
+
+
